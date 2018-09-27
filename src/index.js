16743f5ba575125feb0a8da692f688758a09f08b
SBurnt <zip2004zip@gmail.com>
2018-09-28 01:02:11 +0300

    task additional_5 Brackets first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let check, configStr;
+
+  while (true) {
+    check = false;
+    bracketsConfig.forEach(config => {
+      configStr = config.join('');
+      if (str.includes(configStr)) {
+        str = str.replace(configStr, '');
+        check = true;
+      }
+    });
+    if (!check) break;
+  }
+
+  return (str.length === 0) ? true : false;
 }
